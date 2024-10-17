edd6ef7d45e59309123a57a5cbcd8bd76d94a900
MDBBee <bobbyugbebor@gmail.com>
2024-10-17 13:22:36 +0300

    DBM for managing new users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
+  const token = user.createJWT();
 
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
   console.log(req.body);
