dca9863c48e1c6e318b53c781e8e6e8b66955f75
MDBBee <bobbyugbebor@gmail.com>
2024-10-18 05:58:54 +0300

    CRUD basic functionality completed

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,67 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+} = require("../errors/index");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-  res.send("get all jobs");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) throw new NotFoundError(`No job with the id: ${jobId} was found!`);
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 const updateJob = async (req, res) => {
-  res.send("get all jobs");
+  const {
+    user: { userId },
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (!company || !position)
+    throw new BadRequestError(
+      "Please provide values for company and position!"
+    );
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!job) throw new NotFoundError(`No job with the id: ${jobId} was found!`);
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deletJob = async (req, res) => {
-  res.send("get all jobs");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId });
+  if (!job) throw new NotFoundError(`No job with the id: ${jobId} was found!`);
+  res.status(StatusCodes.OK).json();
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deletJob };
